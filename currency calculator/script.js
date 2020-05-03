dfb1d9dc884b5863a65f1c9320333a27df2cbcdc
marcin-bojar <marcin.bojar@interia.pl>
2020-05-03 21:58:55 +0200

    Async/await + error handling

--- currency calculator/script.js
+++ currency calculator/script.js
@@ -5,21 +5,54 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-function calculate() {
+async function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
-  
-    fetch(`https://api.exchangeratesapi.io/latest?base=${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-        //Get the exchange rate for selected currencies
-        const rate = data.rates[currency_two];
+    
+    try {
+        const res = await fetch(`https://api.exchangeratesapi.io/latest?base=${currency_one}`);
+        const result = await res.json();
+
+        // Check if base currency is supported by API
+        if(!result.rates) {
+            alert(result.error)
         
-        //Update UI with data
-        rateEl.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
-        amountEl_two.value = (+amountEl_one.value * rate).toFixed(4);
-    });  
-}
+        // Check if selected currency pair is supported by API     
+        }else if(!result.rates[currency_two]){
+            alert('This currency pair is not supported');
+            
+        } else {
+            //Get the exchange rate for selected currencies
+            const rate = result.rates[currency_two];
+        
+            //Update UI with data
+            rateEl.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
+            amountEl_two.value = (+amountEl_one.value * rate).toFixed(4);
+        }
+        
+    } catch (error) {
+        alert(error);
+        console.log(error);
+    }
+
+    // fetch(`https://api.exchangeratesapi.io/latest?base=${currency_one}`)
+    // .then(res => res.json())
+    // .then(data => {
+    //     //Get the exchange rate for selected currencies
+    //     console.log(data);
+    //     const rate = data.rates[currency_two];
+        
+    //     //Update UI with data
+    //     rateEl.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
+    //     amountEl_two.value = (+amountEl_one.value * rate).toFixed(4);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     alert(err);
+    // });  
+    
+
+};
 
 function swapCurrency() {
     const temp = currencyEl_one.value;
