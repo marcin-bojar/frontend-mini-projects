88e69d26cc40c2bf5589452658f6f7295dd2450e
marcin-bojar <marcin.bojar@interia.pl>
2020-06-02 21:31:58 +0200

    Add and delete transaction

--- expense tracker/script.js
+++ expense tracker/script.js
@@ -2,13 +2,16 @@ const list = document.getElementById('list');
 const balance = document.getElementById('balance');
 const income = document.getElementById('income');
 const expense = document.getElementById('expense');
+const description = document.getElementById('text');
+const amount = document.getElementById('amount');
+const form = document.getElementById('form');
 
 const temporaryTransactions = [
-    {id:1, text: 'Fuel', amount: -80 },
-    {id:2, text: 'Salary', amount: 1080 },
-    {id:3, text: 'Food', amount: -189 },
-    {id:4, text: 'Lottery', amount: 180 },
-    {id:5, text: 'Rent', amount: -780.20 }
+    // {id:1, text: 'Fuel', amount: -80 },
+    // {id:2, text: 'Salary', amount: 1080 },
+    // {id:3, text: 'Food', amount: -189 },
+    // {id:4, text: 'Lottery', amount: 180 },
+    // {id:5, text: 'Rent', amount: -780.20 }
 ];
 
 // Data container
@@ -27,6 +30,7 @@ function addTransactionDOM(transaction) {
 
     const item = document.createElement('li');
     item.classList.add('list__item', transaction.amount < 0 ? 'minus' : 'plus');
+    item.setAttribute('data-id', `${transaction.id}`)
     item.innerHTML = `
         ${transaction.text}<span>${sign}${Math.abs(transaction.amount).toFixed(2)}</span><button class="delete-btn">X</button>
     `;
@@ -34,18 +38,73 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 };
 
+// Calculate Budget balance, total income and total expense
 function calcTotals() {
-    data.transactions.forEach( el => {
-        el.amount > 0 ? data.totals.inc += el.amount : data.totals.exp += el.amount;
-    });
+    data.totals.inc = data.transactions
+        .filter( el => el.amount > 0)
+        .reduce((acc, el) => acc += el.amount, 0);
+   
+    data.totals.exp = data.transactions
+        .filter( el => el.amount < 0)
+        .reduce((acc, el) => acc += el.amount, 0);
+
     data.budget = data.totals.inc - Math.abs(data.totals.exp);
 };
 
+// Render data values in UI
 function displayTotals() {
     balance.innerText = `$${data.budget.toFixed(2)}`;
     income.innerText = `$${data.totals.inc.toFixed(2)}`;
     expense.innerText = `$${Math.abs(data.totals.exp).toFixed(2)}`;
 };
+
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+};
+
+// Add transaction to data container and display in UI
+function addTransaction(e) {
+    let transaction;
+    e.preventDefault();
+    // Get input values
+    if(description.value.trim() !== '' && amount.value.trim() !== '') {
+        transaction = {
+            id: generateID(),
+            text: description.value,
+            amount: parseFloat(amount.value)
+        }
+    }
+
+    if(transaction) {
+        // Add data to data container
+        data.transactions.push(transaction);
+        // Render new transaction
+        addTransactionDOM(transaction);
+        // Update values
+        calcTotals();
+        displayTotals();
+        // Clear input
+        description.value = '';
+        amount.value = '';
+    }
+    
+};
+
+function removeTransaction(e) {
+
+    if(e.target.classList.contains('delete-btn')) {
+        // Get the id of transaction
+        const ID = +e.target.parentNode.dataset.id;
+        // Remove item from UI
+        list.removeChild(e.target.parentNode);
+        // Remove transaction from data container
+        data.transactions = data.transactions.filter(el => el.id !== ID);
+        // Update values and UI
+        calcTotals();
+        displayTotals();
+    }
+    
+}
 
 function init() {
     list.innerHTML = '';
@@ -55,3 +114,7 @@ function init() {
 };
 
 init();
+
+// Event listeners
+form.addEventListener('submit', addTransaction);
+document.addEventListener('click', removeTransaction);
