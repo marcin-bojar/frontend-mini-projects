b896c118a15577abc5696dac8144bf217d7c9e98
marcin-bojar <marcin.bojar@interia.pl>
2020-06-10 22:47:09 +0200

    New Year Counter: loader

--- new year counter/script.js
+++ new year counter/script.js
@@ -1,14 +1,18 @@
-const year = document.getElementById('year');
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('mins');
 const seconds = document.getElementById('secs');
+const container = document.getElementById('container');
+const loader = document.getElementById('loader');
+const year = document.getElementById('year');
 
+// Get the next year value
 const nextYear = new Date().getFullYear() + 1;
 const newYearTime = new Date(`01-01-${nextYear} 00:00:00 UTC+02:00`);
-console.log(newYearTime);
+// Add the next year value to the DOM
 year.innerText = nextYear;
 
+// Count the time to New Year
 function count() {
     const now = new Date();
     const difference = newYearTime - now;
@@ -20,6 +24,7 @@ function count() {
     updateUI(d, h, m, s);
 };
 
+// Update DOM with new values
 function updateUI(d, h, m, s) {
     days.innerText = d;
     hours.innerText = h < 10 ? '0' + h : h;
@@ -27,5 +32,14 @@ function updateUI(d, h, m, s) {
     seconds.innerText = s < 10 ? '0' + s : s;
 };
 
+// Remove spinner and display counter when counter data is ready
+function whenDataReady() {
+    loader.remove();
+    container.style.display = 'flex';
+    year.style.display = 'block';
+};
 
+setTimeout(whenDataReady, 1000);
+
+// Update counter every second
 setInterval(count, 1000);
