f461497895f1939cdd4b227179208837db65e1c7
marcin-bojar <marcin.bojar@interia.pl>
2020-06-25 20:58:24 +0200

    LyricsSearch: display fetched songs in UI

--- lyrics search/script.js
+++ lyrics search/script.js
@@ -2,17 +2,41 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+const list = document.getElementById('list');
 
 async function searchSongs(term) {
     const res = await fetch(`https://api.lyrics.ovh/suggest/${term}`);
     const data = await res.json();
     
-    // console.log(data);
+    console.log(data);
     renderResults(data);
 };
 
-function renderResults(data) {
+async function getSongLyrics(artist, title) {
+    const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
+    const data = await res.json();
+    console.log(data);
+    list.innerHTML = `${data.lyrics}`;
+};
 
+function parseTitle(title) {
+    return title.replace(/'/g, '');
+};
+
+function renderResults(data) {
+    // Clear previous search results
+    list.innerHTML = '';
+    
+    let title;
+    data.data.forEach(el => {
+        title = parseTitle(el.title);
+        
+        const listItem = `
+            <li class="songs-list__item" onclick = "getSongLyrics('${el.artist.name}', '${title}')">${el.artist.name} - ${el.title}</li>
+        `;
+
+        list.insertAdjacentHTML('beforeend', listItem);
+    });
 };
 
 //Event listeners
